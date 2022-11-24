e37418aa9a8ddfb5470003d00f2364c6fee46242
priyamkhatri <priyam.k@ingeniumedu.com>
2022-11-24 11:16:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+if (
+  navigator.userAgent.match(
+    /SAMSUNG|SGH-[I|N|T]|GT-[I|P|N]|SM-[N|P|T|Z|G]|SHV-E|SCH-[I|J|R|S]|SPH-L/i
+  )
+) {
+  alert("it's Samsung default browser");
+  // your code for Samsung goes here
+  window.location.href = "https://testing.ingeniumedu.com";
+}
+
+function iOS() {
+  return (
+    [
+      "iPad Simulator",
+      "iPhone Simulator",
+      "iPod Simulator",
+      "iPad",
+      "iPhone",
+      "iPod",
+    ].includes(navigator.platform) ||
+    // iPad on iOS 13 detection
+    (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  );
+}
+
+if (iOS()) {
+  window.location.href = "https://www.apple.com/in/";
+} else {
+  window.location.href = "https://www.ingeniumedu.com";
+}
